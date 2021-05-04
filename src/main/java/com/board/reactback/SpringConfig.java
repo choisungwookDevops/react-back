44ab743582a7fdcde6a872034487e2c2f6c8f936
chlwlgus4 <chlwlgus89@naver.com>
2021-05-05 01:02:55 +0900

    마리아디비 추가

--- src/main/java/com/board/reactback/SpringConfig.java
+++ src/main/java/com/board/reactback/SpringConfig.java
@@ -1,15 +1,36 @@
 package com.board.reactback;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.board.reactback.posts.repository.PostsRepository;
 import com.board.reactback.posts.service.PostsService;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class SpringConfig {
 
     @Bean PostsService postsService(PostsRepository postsRepository) {
         return new PostsService(postsRepository);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    //③
+    @Bean
+    public DataSource dataSource() throws Exception {
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        System.out.println(dataSource.toString());
+        return dataSource;
     }
 }
